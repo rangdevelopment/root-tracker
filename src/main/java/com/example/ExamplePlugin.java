18d33e51adf2cb1565970d6007529711e2d19440
rangdevelopment <138425698+rangdevelopment@users.noreply.github.com>
2023-07-03 04:17:45 -0700

    first commit
    functional
    timers for different locations
    expiration
    warning before timer ends
    (need to add settings)

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -1,16 +1,22 @@
 package com.example;
 
+import java.time.Duration;
+import java.util.HashMap;
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import java.time.Instant;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.*;
+import net.runelite.client.game.ItemManager;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
+import net.runelite.api.events.*;
+import net.runelite.client.ui.overlay.infobox.InfoBoxPriority;
+
 
 @Slf4j
 @PluginDescriptor(
@@ -19,10 +25,22 @@ import net.runelite.client.plugins.PluginDescriptor;
 public class ExamplePlugin extends Plugin
 {
 	@Inject
+	private InfoBoxManager infoBoxManager;
+
+	@Inject
+	private ItemManager itemManager;
+
+	@Inject
 	private Client client;
 
 	@Inject
 	private ExampleConfig config;
+
+	private long long_dur = 120;
+	private Integer roots_size = Root.SMAGE.getSize();
+	private String msg_short;
+	private HashMap<String, Instant> active_roots = new HashMap<String, Instant>();
+
 
 	@Override
 	protected void startUp() throws Exception
@@ -34,15 +52,107 @@ public class ExamplePlugin extends Plugin
 	protected void shutDown() throws Exception
 	{
 		log.info("Example stopped!");
+		infoBoxManager.removeIf(t -> t instanceof ExampleTimer);
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onChatMessage(ChatMessage chatMessage)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+
+		if (chatMessage.getSender() == null) {
+			return;
 		}
+
+		if (chatMessage.getSender().toLowerCase().contains("root")) {
+
+			String msg = chatMessage.getMessage().toLowerCase();
+			if (msg.length() > 12) {
+				msg_short = msg.substring(0, 12);
+			} else {
+				msg_short = msg;
+			}
+
+			Root event = Root.find(msg_short);
+
+			if (event != null) {
+				displayTimer(event, msg);
+			}
+
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick event) {
+		if (client.getTickCount() % 10 == 0) {  // every 6 seconds roughly
+			for (String key : active_roots.keySet()) {
+				// check if expired
+				if (Duration.between(active_roots.get(key), Instant.now()).toSeconds() > long_dur) {
+					active_roots.remove(key);
+				}
+				// if 80 secs, it's probably too late
+				if (Duration.between(active_roots.get(key), Instant.now()).toSeconds() > 80) {
+					infoBoxManager.removeIf(t -> t instanceof ExampleTimer && ((ExampleTimer) t).getEvent().equals(key));
+					long new_duration = Duration.between(active_roots.get(key), Instant.now()).toSeconds() - long_dur;
+					ExampleTimer timer = new ExampleTimer(key, new_duration, itemManager.getImage(ItemID.TOMATO), this);
+					timer.setTooltip(key);
+					timer.setPriority(InfoBoxPriority.HIGH);
+					infoBoxManager.addInfoBox(timer);
+				}
+				// alive
+				log.info(key + " alive " + Duration.between(active_roots.get(key), Instant.now()).toSeconds() + " sec");
+			}
+		}
+	}
+
+	private void displayTimer(Root event, String msg) {
+
+		if (event.isConfirmed(msg)) {
+			log.info(event.getName() + " confirmed.");
+
+			if (!active_roots.containsKey(event.getName())) {
+				infoBoxManager.removeIf(t -> t instanceof ExampleTimer && ((ExampleTimer) t).getEvent().equals(event.getName()));
+				ExampleTimer timer = new ExampleTimer(event.getName(), long_dur, itemManager.getImage(event.getItemSpriteId()), this);
+				timer.setTooltip(event.getName());
+				timer.setPriority(InfoBoxPriority.HIGH);
+				infoBoxManager.addInfoBox(timer);
+				active_roots.put(event.getName(),Instant.now());
+			}
+
+
+		} else if (event.isDead(msg)) {
+			if (active_roots.containsKey(event.getName())) {
+				long duration = Duration.between(active_roots.get(event.getName()), Instant.now()).toSeconds();
+				log.info(event.getName() + " dead after " + duration + "sec");
+			} else {
+				log.info(event.getName() + " dead." );
+			}
+
+			infoBoxManager.removeIf(t -> t instanceof ExampleTimer && ((ExampleTimer) t).getEvent().equals(event.getName()));
+			active_roots.remove(event.getName());
+
+
+		} else if (event.isFake(msg)) {
+			log.info(event.getName() + " fake.");
+
+			infoBoxManager.removeIf(t -> t instanceof ExampleTimer && ((ExampleTimer) t).getEvent().equals(event.getName()));
+			active_roots.remove(event.getName());
+
+
+		} else {
+			log.info(event.getName());
+
+			if (!active_roots.containsKey(event.getName())) {
+				infoBoxManager.removeIf(t -> t instanceof ExampleTimer && ((ExampleTimer) t).getEvent().equals(event.getName()));
+				ExampleTimer timer = new ExampleTimer(event.getName(), long_dur, itemManager.getImage(event.getItemSpriteId()), this);
+				timer.setTooltip(event.getName());
+				timer.setPriority(InfoBoxPriority.HIGH);
+				infoBoxManager.addInfoBox(timer);
+				active_roots.put(event.getName(),Instant.now());
+			}
+
+
+		}
+
 	}
 
 	@Provides
@@ -50,4 +160,5 @@ public class ExamplePlugin extends Plugin
 	{
 		return configManager.getConfig(ExampleConfig.class);
 	}
+
 }
---
+++ src/main/java/com/example/RootLocations.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2016-2017, Cameron Moberg <Moberg@tuta.io>
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.example;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import net.runelite.api.ItemID;
+
+
+enum Root {
+	NMAGE("North Sorcerer's Tower (Nmage)", "nmage", null, null, ItemID.COMBAT_BRACELET),
+	SMAGE("South Sorcerer's Tower (Smage)", "smage", null, null, ItemID.CAPE_OF_LEGENDS),
+	DRAY("Draynor (Dray)", "dray", null, null, ItemID.AMULET_OF_GLORY),
+	CHURCH("Seers Church (Church)", "chur", null, null, ItemID.HOLY_SYMBOL),
+	NSEERS("North Seers (NSeers)", "seer", "n", null, ItemID.MAPLE_LOGS),
+	SEERS("Seers Bank", "seer", null, "n", ItemID.CAMELOT_TELEPORT),
+	GLADE("Xeric's Glade (Glade)", "glade", null, null, ItemID.XERICS_TALISMAN),
+	BEE("Seers Bees (Bees)", "bee", null, null, ItemID.BEE_ON_A_STICK),
+	ZALC("Zalcano (Zalc)", "zalc", null, null, ItemID.ZALCANO_SHARD),
+	MYTH("Myth's Guild (Myth)", "myth", null, null, ItemID.MYTHICAL_CAPE_22114),
+	ARC("Arceuus Magics (Arc)", "arc", null, null, ItemID.BOOK_OF_THE_DEAD),
+	PRIFF_T("Prifddinas Teaks (Prif Teaks)", "prif", "t", null, ItemID.TEAK_LOGS),
+	PRIFF_M("Prifddinas Mahogany (Prif Mahog)", "prif", "m", null, ItemID.MAHOGANY_LOGS),
+	YAK("Neitiznot (Yak)", "yak", null, null, ItemID.HELM_OF_NEITIZNOT),
+	;
+
+	private static final Map<String, Root> roots;
+
+	private final String name;
+	private final String filter1;
+	private final String filter2;
+	private final String exclude;
+	private final int itemSpriteId;
+
+	static {
+		ImmutableMap.Builder<String, Root> builder = new ImmutableMap.Builder<>();
+
+		for (Root roots : values()) {
+			builder.put(roots.getName(), roots);
+		}
+
+		roots = builder.build();
+	}
+
+	Root(String name, String filter1, String filter2, String exclude, int itemSpriteId) {
+		this.name = name;
+		this.filter1 = filter1;
+		this.filter2 = filter2;
+		this.exclude = exclude;
+		this.itemSpriteId = itemSpriteId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getSize() {
+		return roots.size();
+	}
+
+	public boolean isMatch(String msg) {
+		if (exclude != null) {
+			if (msg.contains(exclude)) {
+				return false;
+			}
+		}
+
+		if (filter2 != null) {
+			if (!msg.contains(filter2)) {
+				return false;
+			}
+		}
+
+		if (msg.contains(filter1)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isConfirmed(String msg) {
+		if (filter1 != null) {
+			msg = msg.replace(filter1, "");
+		}
+		if (filter2 != null) {
+			msg = msg.replace(filter2, "");
+		}
+		return msg.contains("c") || msg.contains("real");
+		// c, con, con*, conf, confirmed, real
+	}
+
+	public boolean isDead(String msg) {
+		if (filter1 != null) {
+			msg = msg.replace(filter1, "");
+		}
+		if (filter2 != null) {
+			msg = msg.replace(filter2, "");
+		}
+		return msg.contains("d") || msg.contains("clear") || msg.contains("bust");
+		// dead, d, ded, done, clear, cleared, bust, busted
+	}
+
+	public boolean isFake(String msg) {
+		if (filter1 != null) {
+			msg = msg.replace(filter1, "");
+		}
+		if (filter2 != null) {
+			msg = msg.replace(filter2, "");
+		}
+		return msg.contains("fake");
+		// fake
+	}
+
+	public int getItemSpriteId() {
+		return itemSpriteId;
+	}
+
+	public static Root find(String msg) {
+		for (String key : roots.keySet()) {
+			if (roots.get(key).isMatch(msg)) {
+				return roots.get(key);
+			}
+		}
+		return null;
+	}
+
+}
